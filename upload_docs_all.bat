1b0a95f3482eac8dca9437e1eec2b2cd5209009e
Corak <doomerovich@gmail.com>
2017-10-01 15:19:25 +0300

    Log creation logic

--- upload_docs_all.bat
+++ upload_docs_all.bat
@@ -8,9 +8,9 @@ set groupid=
 set token=0
 set tokenparam=-k %token%
 if %token% EQU 0 set tokenparam=
-if not exist vkdocs_log.txt echo Log Start>vkdocs_log.txt
 for %%G in (%ext%) do (
 "%iojs%" "%script%" --file "%%G" -g %groupid% %tokenparam% --no-encode-progress>temp.txt 
 type temp.txt 
+if not exist vkdocs_log.txt echo Log Start>vkdocs_log.txt
 copy /b vkdocs_log.txt+temp.txt vkdocs_log.txt)
 pause
